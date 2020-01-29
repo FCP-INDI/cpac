ae9c419cff0dfdfe91c22b62d5cb87e0a4f39137
Jon Clucas <jon.clucas@childmind.org>
2020-01-29 16:37:16 -0500

    :construction: :tada: Somewhat working!
    1. Clone repo
    2. `cd` into local copy
    3. `pip install .`
    4. `cpac run --help` to see options
    
    Still fairly crash-prone.
    Still needs to add support for utils.
    
    on-behalf-of: @FCP-INDI <http://fcp-indi.github.com>

--- src/cpac/__main__.py
+++ src/cpac/__main__.py
@@ -7,6 +7,7 @@ import os
 import sys
 
 from cpac import __version__
+from cpac.backends.docker import Docker, DockerRun
 
 _logger = logging.getLogger(__name__)
 
@@ -68,14 +69,25 @@ def parse_args(args):
     run_parser.add_argument('--address', action='store', type=address)
     run_parser.add_argument('--bids_dir', help="input dataset directory", metavar="PATH")
     run_parser.add_argument('--output_dir', default='/outputs', help="directory where output files should be stored", metavar="PATH")
+    run_parser.add_argument(
+        '--data_config_file',
+        help="Yaml file containing the location of the data that is to be "
+             "processed.",
+        metavar="PATH"
+    )
+    run_parser.add_argument('--tag', help="tag of the container image to use (eg, latest or nightly)")
     # run_parser.add_argument('--backend', choices=['docker']) # TODO: Add Singularity
     run_parser.add_argument('level_of_analysis', choices=['participant', 'group', 'test_config'])
-    run_parser.add_argument('args', nargs=argparse.REMAINDER, help="any C-PAC optional arguments <http://fcp-indi.github.io/docs/user/quick>")
+    run_parser.add_argument('extra_args', nargs=argparse.REMAINDER, help="any C-PAC optional arguments <http://fcp-indi.github.io/docs/user/running>")
 
-    parsed = parser.parse_args(args)
+    parsed, extras = parser.parse_known_args(args)
 
     # Set backend automatically while there's only one supported backend
     parsed.backend = 'docker'
+    parsed.extra_args = [
+        *(parsed.extra_args if hasattr(parsed, 'extra_args') else []),
+        *extras
+    ]
 
     return parsed
 
@@ -89,35 +101,49 @@ def setup_logging(loglevel):
 def main(args):
     command = args[0]
     args = parse_args(args[1:])
+
+    args.data_config_file = args.data_config_file if hasattr(
+        args,
+        'data_config_file'
+    ) else None
+
+    args.bids_dir = args.bids_dir if hasattr(
+        args,
+        'bids_dir'
+    ) else None
+
+
+    if ((args.bids_dir is None) and (args.data_config_file is None)):
+        raise AttributeError(
+            "cpac requires at least one of `bids_dir` or `data_config_file` to "
+            "run. See http://fcp-indi.github.io/docs/user/running"
+        )
+
     setup_logging(args.loglevel)
 
-    if args.command == 'scheduler':
-        from cpac.scheduler.process import start_scheduler
-        start_scheduler(args.address, args.backend)
+    if args.command == 'run':
 
-    elif args.command == 'run':
-
-        if not args.address:
-            from cpac.scheduler.process import spawn_scheduler
-            spawn_scheduler(args.address, args.backend)
-
-        from cpac.scheduler.client import schedule, wait
-        from cpac.scheduler import SCHEDULER_ADDRESS
-
-        scheduler = args.address or SCHEDULER_ADDRESS
-
-        schedule(
-            scheduler,
-            args.backend,
-            args.data_config_file if hasattr(
-                args,
-                'data_config_file'
-            ) else None,
-            args.pipeline_file if hasattr(
-                args,
-                'pipeline_file'
-            ) else None
-        )
+        Docker().run(flags=" ".join(args.extra_args), **vars(args))
+        # DockerRun()
+        #
+        # if not args.address:
+        #     from cpac.scheduler.process import spawn_scheduler
+        #     spawn_scheduler(args.address, args.backend)
+        #
+        # from cpac.scheduler.client import schedule, wait
+        # from cpac.scheduler import SCHEDULER_ADDRESS
+        #
+        # scheduler = args.address or SCHEDULER_ADDRESS
+        #
+        # schedule(
+        #     scheduler,
+        #     args.backend,
+        #     args.data_config_file,
+        #     args.pipeline_file if hasattr(
+        #         args,
+        #         'pipeline_file'
+        #     ) else None
+        # )
 
 
 def run():
--- src/cpac/backends/docker.py
+++ src/cpac/backends/docker.py
@@ -21,18 +21,60 @@ from tornado import httpclient
 
 class Docker(Backend):
 
-    tag = 'nightly'
-
-    def __init__(self, scheduler):
+    def __init__(self):
         self.client = docker.from_env()
+        print('a')
         try:
             self.client.ping()
         except docker.errors.APIError:
             raise "Could not connect to Docker"
-        self.scheduler = scheduler
 
-    def schedule(self, pipeline, data_config):
-        self.scheduler.schedule(DockerSchedule(self, pipeline, data_config))
+    def run(self, tag='nightly', flags="", **kwargs):
+
+        volumes = {
+            kwargs['temp_dir']: {'bind': '/scratch', 'mode': 'rw'},
+            kwargs['output_dir']: {'bind': '/outputs', 'mode':'rw'},
+            kwargs['working_dir']: {'bind': '/wd', 'mode': 'rw'}
+        }
+
+        if isinstance(
+            kwargs['bids_dir'], str
+        ) and not kwargs['bids_dir'].startswith('s3://'):
+            bids_dir = '/bids_dir'
+            volumes[kwargs['bids_dir']] = {'bind': bids_dir, 'mode': 'ro'}
+        else:
+            bids_dir = str(kwargs['bids_dir'])
+
+        command = [i for i in [
+            bids_dir,
+            '/outputs',
+            kwargs['level_of_analysis'],
+            *flags.split(' ')
+        ] if (i is not None and len(i))]
+
+        print(command)
+        print(volumes)
+        print(tag)
+        print(':'.join([
+            'fcpindi/c-pac',
+            tag if isinstance(tag, str) else 'nightly'
+        ]))
+
+        _run = DockerRun(self.client.containers.run(
+            ':'.join([
+                'fcpindi/c-pac',
+                tag if isinstance(tag, str) else 'nightly'
+            ]),
+            command=command,
+            detach=False,
+            user=os.getuid(),
+            volumes=volumes,
+            working_dir='/wd'
+        ))
+
+        print('a')
+        _run.container.wait()
+        print('b')
 
 
 class DockerRun(object):
@@ -117,7 +159,6 @@ class DockerSchedule(Schedule):
                     parent=self
                 )
             )
-
 
 class DockerSubjectSchedule(DockerSchedule):
 
