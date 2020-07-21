7e5b805eba9d8a0d584c5597226633f9ff996ad5
Anibal Solon <anibalsolon@gmail.com>
2020-07-20 20:39:47 -0400

    Docker socket used for testing docker log messaging

--- tests/test_data/entrypoint.py
+++ tests/test_data/entrypoint.py
@@ -2,19 +2,27 @@
 # -*- coding: utf-8 -*-
 
 import argparse
+import asyncio
 import json
 import logging
 import os
 import shutil
-import socketserver
 import sys
 import threading
+import time
 
 import yaml
 
+import websockets
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger('websockets')
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler())
+
 __version__ = '0.0.0'
 DEFAULT_PIPELINE = "/cpac_resources/default_pipeline.yml"
-
 
 parser = argparse.ArgumentParser(description='C-PAC Pipeline Runner')
 parser.add_argument('bids_dir')
@@ -56,6 +64,8 @@ args = parser.parse_args(
     ]
 )
 
+print(args)
+
 if args.analysis_level == "cli":
     more_options = sys.argv[sys.argv.index('--') + 1:]
     if more_options[0:3] == ['utils', 'data_config', 'build']:
@@ -73,44 +83,60 @@ elif args.analysis_level == "test_config":
     sys.exit(0)
 
 elif args.analysis_level == "participant":
-    data_config = yaml.safe_load(args.data_config_file)
+    data_config = yaml.safe_load(open(args.data_config_file))[0]
 
+    nodes = [
+        f"resting_preproc_sub-{data_config['subject_id']}.anat_pipeline.normalize",
+        f"resting_preproc_sub-{data_config['subject_id']}.anat_pipeline.skullstrip",
+        f"resting_preproc_sub-{data_config['subject_id']}.anat_pipeline.segmentation",
+        f"resting_preproc_sub-{data_config['subject_id']}.func_pipeline.skullstrip",
+        f"resting_preproc_sub-{data_config['subject_id']}.func_pipeline.registration",
+        f"resting_preproc_sub-{data_config['subject_id']}.func_pipeline.nuisance_regression",
+    ]
 
-    class LoggingRequestHandler(socketserver.BaseRequestHandler):
+    sleep_between_nodes = 0.5
 
-        def handle(self):
+    async def socketee(websocket, path):
+        if path != '/log':
+            return
 
-            tree = {}
+        start = time.time()
 
-            # self.server.pipeline_name
-            for sub_config in self.server.data_config:
-                tree[sub_config['subject_id']] = {
-                    "node-00001": {
-                        "hash": "00000000000000000000000000000000",
-                        "start": "2020-01-01T10:00:00",
-                        "finish": "2020-01-01T10:10:00",
-                    }
+        for i, node in enumerate(nodes):
+            start = time.time()
+            await asyncio.sleep(0.5)
+
+            await websocket.send(json.dumps({
+                "time": time.time(),
+                "message": {
+                    "id": node,
+                    "hash": f"{i:032}",
+                    "start": start,
+                    "finish": time.time(),
                 }
+            }))
 
-            headers = '''
-HTTP/1.1 200 OK
-Connection: close
+            logger.info(f"{node}")
 
-'''
-            self.request.sendall(headers + json.dumps(tree) + "\n")
+        # Keep socket open
+        while True:
+            await asyncio.sleep(1)
 
+    async def serve():
+        try:
+            server = await websockets.serve(socketee, "0.0.0.0", 8008)
+            await server.wait_closed()
+        finally:
+            server.close()
 
-    class LoggingHTTPServer(socketserver.ThreadingTCPServer):
-        def __init__(self, pipeline_name, data_config='', host='', port=8080, request=LoggingRequestHandler):
-            super(LoggingHTTPServer, self).__init__((host, port), request)
-            self.pipeline_name = pipeline_name
-            self.data_config = data_config
+    async def wait_some():
+        await asyncio.sleep(len(nodes) * sleep_between_nodes * 5)
 
-    def monitor_server(pipeline_name, data_config, host='0.0.0.0', port=8080):
-        httpd = LoggingHTTPServer(pipeline_name, data_config, host, port, LoggingRequestHandler)
-        server_thread = threading.Thread(target=httpd.serve_forever)
-        server_thread.isDaemon = True
-        server_thread.start()
-        return server_thread
+    async def main():
+        task = asyncio.create_task(serve())
+        wait = asyncio.create_task(wait_some())
+        await asyncio.wait([task, wait], return_when=asyncio.FIRST_COMPLETED)
+
+    asyncio.run(main(), debug=True)
 
     sys.exit(0)
