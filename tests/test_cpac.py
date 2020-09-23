e93bba24724235a6fde4062dd2728872eac12a19
Jon Clucas <jon@vasegurt.com>
2020-09-23 18:33:12 -0400

    :white_check_mark: Fix loading message tests

--- tests/test_cpac.py
+++ tests/test_cpac.py
@@ -1,22 +1,28 @@
 import pytest
-import sys
 
+from contextlib import redirect_stdout
 from cpac.backends import Backends
+from io import StringIO, TextIOWrapper, BytesIO
 
 
 @pytest.mark.parametrize('platform', ['docker', 'singularity'])
-def test_loading_message(capsys, platform):
-    loaded = Backends(platform)
-    captured = capsys.readouterr()
-    assert captured.out.split('\n')[0].strip() == ' '.join([
+def test_loading_message(platform):
+    redirect_out = StringIO()
+    with redirect_stdout(redirect_out):
+        loaded = Backends(platform)
+    with_symbol = ' '.join([
         'Loading',
         loaded.platform.symbol,
         loaded.platform.name
     ])
-    sys.stdout.reconfigure(encoding='latin-1')
-    loaded = Backends(platform)
-    captured = capsys.readouterr()
-    assert captured.out.split('\n')[0].strip() == ' '.join([
+    assert with_symbol in redirect_out.getvalue()
+
+    redirect_out = TextIOWrapper(
+        BytesIO(), encoding='latin-1', errors='strict', write_through=True)
+    with redirect_stdout(redirect_out):
+        loaded = Backends(platform)
+    without_symbol = ' '.join([
         'Loading',
         loaded.platform.name
     ])
+    assert without_symbol in redirect_out.buffer.getvalue().decode()
