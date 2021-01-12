b9f16e39112e62f6bf6073cf3e9a3fa023a3a239
AWS ParallelCluster user <ubuntu@ip-10-0-0-192.ec2.internal>
2021-01-12 23:02:09 +0000

    start script for creating a cpac cluster ami

---
+++ ami-software-setup.sh
@@ -0,0 +1,9 @@
+# install miniconda
+curl https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh --output /tmp/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh -b -f 
+
+# install cpac
+
+# install singularity
+
+# install docker
