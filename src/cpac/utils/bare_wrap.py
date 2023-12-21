2b71900f51f79994702570719e9d031bc83d4934
Jon Clucas <jon.clucas@childmind.org>
2023-12-21 00:39:54 -0500

    :sparkles: Add `tsconcat` command to `cpac`

--- src/cpac/utils/bare_wrap.py
+++ src/cpac/utils/bare_wrap.py
@@ -11,10 +11,11 @@ from sys import exit as sys_exit, version_info
 from typing import Optional, TypedDict
 
 if version_info.minor < 9:  # noqa: PLR2004
-    from typing import Dict, List
+    from typing import Dict, List, Tuple
 else:
     Dict = dict  # type: ignore
     List = list  # type: ignore
+    Tuple = tuple  # type: ignore
 
 from packaging.requirements import Requirement
 
@@ -23,8 +24,47 @@ try:
 except (ImportError, ModuleNotFoundError):
     gradients_parser = None
 
+try:
+    from tsconcat.utils import build_bidsapp_group_parser
+except (ImportError, ModuleNotFoundError):
+    tsconcat_parser = build_bidsapp_group_parser = None
+
 from cpac.utils import INTERVAL_CHECKS, version_tuple
 
+if build_bidsapp_group_parser is not None:
+    """
+    This parser is built in the main function of the wrapped package https://github.com/cmi-dair/tsconcat/blob/a7e31880431621fe47f48664df4736eb7a455859/src/tsconcat/cli.py#L78C14-L104 , so we recreate it here to generate the usage string for the wrapper."""
+    from tsconcat.cli import REDUCE_COLUMNS
+
+    tsconcat_parser = build_bidsapp_group_parser(
+        prog="ba-tsconcat", description="Concatenate MRI timeseries."
+    )
+    tsconcat_parser.add_argument(
+        "-c",
+        "--concat",
+        type=str,
+        help=f"Concat across. Can be any combination of {', '.join(REDUCE_COLUMNS)} separated by spaces. "
+        f"Output data will be grouped by the set difference.",
+        default="ses",
+    )
+    tsconcat_parser.add_argument(
+        "-d",
+        "--dry_run",
+        action="store_true",
+        help="Dry run. Print output directory structure instead of actually doing something. "
+        "If this is enabled 'bids_dir' may be a path to a bids2table parquet directory.",
+        default=False,
+    )
+    tsconcat_parser.add_argument(
+        "-f",
+        "--fake",
+        action="store_true",
+        help="Fake output. Output a bids2table parquet directory instead of actually doing something.",
+        default=False,
+    )
+
+_PARSERS = {"gradients": gradients_parser(), "tsconcat": tsconcat_parser}
+"""CLI parsers for wrapped packages"""
 WRAPPED = {}
 """memoization of wrapped packages"""
 
@@ -36,6 +76,8 @@ class ScriptInfo(TypedDict):
     """The command to run."""
     helpstring: Optional[str]
     """The original usage string."""
+    package: str
+    """The canonical name of the wrapped package."""
     positional_arguments: bool
     """Whether the original command takes positional arguments."""
     supported_python: str
@@ -48,10 +90,19 @@ _SCRIPTS: Dict[str, ScriptInfo] = {
     "gradients": {
         "command": "ba_timeseries_gradients",
         "helpstring": None,
+        "package": "ba_timeseries_gradients",
         "positional_arguments": True,
         "supported_python": "[3.11, 3.12)",
         "url": "https://cmi-dair.github.io/ba-timeseries-gradients/ba_timeseries_gradients.html",
-    }
+    },
+    "tsconcat": {
+        "command": "ba-tsconcat",
+        "helpstring": None,
+        "package": "tsconcat",
+        "positional_arguments": True,
+        "supported_python": "[3.11, 4.0)",
+        "url": "https://cmi-dair.github.io/tsconcat/tsconcat.html",
+    },
 }
 """Info needed for bare package wrapping."""
 
@@ -66,6 +117,8 @@ class WrappedBare:
     """The wrapped command."""
     _helpstring: Optional[str]
     """The original usage string."""
+    package: str
+    """The canonical name of the wrapped package."""
     positional_arguments: bool
     """Whether the original command takes positional arguments."""
     supported_python_range: str
@@ -80,13 +133,23 @@ class WrappedBare:
             if not self.supported_python:
                 self._helpstring = f"Extra command '{self.name}' not supported on Python {f'{version_info.major}.{version_info.minor}.{ version_info.micro}'}; supported Python versions range is {self.supported_python_range}.\n\nSee {self.url} for more information and system requirements."
             else:
-                self._helpstring = f"Extra script {self.command} not found. See {self.url} for more information and system requirements, or run `pip install cpac[{self.name}]` to try to install it."
+                self._helpstring = f"Extra package {self.package} not found. See {self.url} for more information and system requirements, or run `pip install cpac[{self.name}]` to try to install it."
         return self._helpstring.replace(f"usage: {self.command}", f"cpac {self.name}")
 
     @property
-    def supported_python(self):
+    def split_range(self) -> Tuple[str, str]:
+        """Split the supported Python range into min and max."""
+        if not hasattr(self, "_split_range"):
+            py_min, py_max = (
+                _.strip() for _ in self.supported_python_range.split(",", 1)
+            )
+            self._split_range = (py_min, py_max)
+        return self._split_range
+
+    @property
+    def supported_python(self) -> bool:
         """Whether the wrapped package supports the current Python version."""
-        py_min, py_max = [_.strip() for _ in self.supported_python_range.split(",")]
+        py_min, py_max = self.split_range
         return getattr(version_info, INTERVAL_CHECKS[py_min[0]])(
             version_tuple(py_min[1:])
         ) and getattr(version_info, INTERVAL_CHECKS[py_max[-1]])(
@@ -118,7 +181,7 @@ def add_bare_wrapper(parser: _SubParsersAction, command: str) -> None:
     bare_parser: ArgumentParser = parser.add_parser(
         command,
         formatter_class=WrappedHelpFormatter,
-        help=f"Run {get_wrapped(command).command}",
+        help=f"Run {get_wrapped(command).package}",
         usage=get_wrapped(command).helpstring,
     )
     bare_parser.add_argument("args", nargs=REMAINDER)
@@ -142,7 +205,7 @@ def call(name: str, command: list) -> None:
     requirements: Dict[str, Requirement] = {req.name: req for req in _requirements}
     del _requirements
     try:
-        package_info = requirements[script.command]
+        package_info = requirements[script.package]
     except KeyError as ke:
         raise KeyError(f"Package {name} not defined in dependencies") from ke
     marker = getattr(package_info, "marker", None)
@@ -163,6 +226,15 @@ def check_for_package(package_name: str) -> bool:
     return which(package_name) is not None
 
 
+def _collect_usage_string(name: str) -> None:
+    """Collect a usage string from a wrapped package to use in the helpstring."""
+    if _PARSERS[name] is not None:
+        try:
+            _SCRIPTS[name]["helpstring"] = _PARSERS[name].format_help()
+        except AttributeError:
+            print(name)
+
+
 def get_wrapped(name: str) -> WrappedBare:
     """Get the name of the script to run.
 
@@ -177,13 +249,13 @@ def get_wrapped(name: str) -> WrappedBare:
         The name of the script to run
     """
     if name not in WRAPPED:
-        if gradients_parser is not None:
-            _SCRIPTS["gradients"]["helpstring"] = gradients_parser().format_help()
+        _collect_usage_string(name)
         if name in _SCRIPTS:
             WRAPPED[name] = WrappedBare(
                 name,
                 _SCRIPTS[name]["command"],
                 _SCRIPTS[name]["helpstring"],
+                _SCRIPTS[name]["package"],
                 _SCRIPTS[name]["positional_arguments"],
                 _SCRIPTS[name]["supported_python"],
                 _SCRIPTS[name]["url"],
